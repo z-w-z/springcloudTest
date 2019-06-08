ded18a066d44ca73942369f2dff8f6346d1ae498
z-w-z <mr_zwzheng@163.com>
2019-06-09 00:05:23 +0800

    通用方式使用Hystrix

---
+++ simple-provider-movie-ribbon-hystrix/src/main/java/com/example/simpleprovidermovie/SimpleProviderMovieApplication.java
@@ -0,0 +1,24 @@
+package com.example.simpleprovidermovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@EnableCircuitBreaker
+@SpringBootApplication
+public class SimpleProviderMovieApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                SimpleProviderMovieApplication.class, args);
+    }
+
+}
